2583f1a06a6148d8dc1ec9c1992f652b6a827726
DevTroyer <tuckeralan.dev@gmail.com>
2020-02-23 23:36:43 -0600

    Add regions to Resources class

--- Compiler/Resources.cs
+++ Compiler/Resources.cs
@@ -7,6 +7,29 @@ namespace Compiler
 {
     public class Resources
     {
+        #region Lexical Analyzer Resources
+
+        // Global regular expressions
+        public static Regex wordTokenRegex = new Regex("[A-Za-z]");
+        public static Regex identifiedWordTokenRegex = new Regex("[0-9A-Za-z_]");
+        public static Regex numTokenRegex = new Regex("[0-9]");
+        public static Regex singleTokenRegex = new Regex(@"[-+*/(){},;.\[\]]");
+        public static Regex doubleTokenRegex = new Regex(@"[=!<>|&]");
+
+        #endregion
+
+        #region Syntax Analyzer Resources
+
+        // Return types
+        public static List<Token> Types = new List<Token>(3)
+        {
+            Token.intt,
+            Token.booleant,
+            Token.voidt
+        };
+
+        #endregion
+
         // Global enumerated date type that holds all of the tokens
         public enum Token
         {
@@ -16,25 +39,11 @@ namespace Compiler
             periodt, quotet, numt, idt, eoft, unknownt
         };
 
-        public static List<Token> Types = new List<Token>(3)
-        {
-            Token.intt,
-            Token.booleant,
-            Token.voidt
-        };
-
         //public static List<Token> variableTypes = new List<Token>(2)
         //{
         //    Token.intt,
         //    Token.booleant
         //};
-
-        // Global regular expressions
-        public static Regex wordTokenRegex = new Regex("[A-Za-z]");
-        public static Regex identifiedWordTokenRegex = new Regex("[0-9A-Za-z_]");
-        public static Regex numTokenRegex = new Regex("[0-9]");
-        public static Regex singleTokenRegex = new Regex(@"[-+*/(){},;.\[\]]");
-        public static Regex doubleTokenRegex = new Regex(@"[=!<>|&]");
 
         // Global variables
         public static List<string> reservedWords = new List<string>(19);
