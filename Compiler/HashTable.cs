bdefa8485ffe3be89ab2601cce0f2f7d9218e197
DevTroyer <tuckeralan.dev@gmail.com>
2020-03-01 14:20:14 -0600

    Implement baseline SymbolTable class and interface

--- Compiler/HashTable.cs
+++
@@ -1,7 +0,0 @@
-﻿
-namespace Compiler
-{
-    class HashTable
-    {
-    }
-}
---
+++ Compiler/SymbolTable.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Compiler
+{
+    class SymbolTable : Resources
+    {
+        /// <summary>
+        /// Inserts the lexemes, token, and depth into a record in the symbol table.
+        /// </summary>
+        public void insert(string lexemes, Token token, int depth)
+        {
+
+        }
+
+        /// <summary>
+        /// Uses the lexeme to find the entry and returns a pointer to that entry. 
+        /// </summary>
+        public void lookup(string lexemes)
+        {
+
+        }
+
+        /// <summary>
+        /// Is passed the depth and deletes all records that are in the table at that depth.
+        /// </summary>
+        public void deleteDepth(int depth)
+        {
+
+        }
+
+        /// <summary>
+        /// Writes out all variables (lexeme only) that are in the table at a specified depth.
+        /// </summary>
+        public void writeTable(int depth)
+        {
+
+        }
+
+        /// <summary>
+        /// Passed a lexemes and returns the location for that lexeme.
+        /// </summary>
+        private void hash(string lexemes)
+        {
+
+        }
+    }
+}
